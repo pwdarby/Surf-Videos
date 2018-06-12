447bf9072da78cb984fc999a0d64993abd1ab90d
Paige Darby <pdarby@wayfair.com>
2018-06-11 21:43:44 -0400

    Hello World - Created first react app

---
+++ src/components/app.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+import '../styles/App.css';
+
+class App extends Component {
+
+  state = {
+    test: []
+  };
+  render() {
+    class test extends Component {
+      state = {
+        test: []
+      };
+    }
+    return (
+      <div>
+        <h1>My React App!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
